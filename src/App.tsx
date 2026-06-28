581055de7ecff38886f9889eb83795ccaee8cdc5
Kunj Patel <kunjp0127@gmail.com>
2026-06-28 16:08:50 -0400

    Good stopping point for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import Nav from "./components/Nav"
+import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <>
       <Nav />
+
+      <div className="font-nunito bg-baseplate min-h-screen">
+        <p className="text-center text-2xl p-8">Portfolio coming soon</p>
+      </div>
     </>
   )
 }
